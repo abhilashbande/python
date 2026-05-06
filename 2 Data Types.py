f1bfa575be288cc9c666f3693b88fb6685e1031b
Abhilash Bande <Abhilash.Bande@avid.com>
2026-05-06 14:48:44 +0530

    data types, type function

---
+++ 2 Data Types.py
@@ -0,0 +1,29 @@
+# Integers
+var = 10;
+print(var)
+
+#type
+print(type(var))
+
+# Floats
+var = 3.15;
+print(type(var))
+
+# Booleans
+var = True;
+print(type(var))
+
+# Strings
+var = "Hello, World!";
+print(type(var))
+
+var = """
+This is multiline string.
+It can span multiple lines.
+""";
+
+print(var)
+
+# None
+var = None;
+print(type(var))
