3973d640ea4392914906293d4660ecd983dd9dbf
Abhilash Bande <Abhilash.Bande@avid.com>
2026-05-06 14:36:33 +0530

    comments and id function

--- 1 Print Function.py
+++ 1 Print Function.py
@@ -2,3 +2,12 @@
 # print("Abhilash", 43, 78463.45, False)
 print("Abhilash", 43, 78463.45, False, sep="\n", end=" - ")
 print("Abhilash", 43, 78463.45, False)
+
+
+# id function
+x = 5;
+print(id(x))
+
+x = "Abhilash"
+print(id(x))
+
