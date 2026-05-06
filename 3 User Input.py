e672e4ab6a58af0b50cf9a0ef859c0f60b8137cd
Abhilash Bande <Abhilash.Bande@avid.com>
2026-05-06 14:58:00 +0530

    user input and type conversion

---
+++ 3 User Input.py
@@ -0,0 +1,11 @@
+
+#input using input() function
+#name = input()
+#print("Hello, " + name + "!");
+
+#type conversion
+x = int(input())
+print("You entered: ", x);
+print("Type of x: " , (type(x)))
+
+
