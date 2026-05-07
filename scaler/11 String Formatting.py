d97f744f3792b038fca48ba14526c1ad9b83e855
Abhilash Bande <Abhilash.Bande@avid.com>
2026-05-07 14:42:39 +0530

    String concatenation

--- scaler/11 String Formatting.py
+++
@@ -1,5 +0,0 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-
-print("Hello, {}! You are {} years old.".format(name, age))
-print(f"Hello, {name}! You are {age} years old.")               # python 3.6 and above
---
+++ scaler/11 String Functions.py
@@ -0,0 +1,13 @@
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+
+# print("Hello, {}! You are {} years old.".format(name, age))
+# print(f"Hello, {name}! You are {age} years old.")               # python 3.6 and above
+
+
+first = "Abhilash"
+last = "Bande"
+
+print("Full name = ", first, last)
+
+print("Abhilash " * 3)
