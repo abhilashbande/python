66eca4415f7e33b6391a4a3bc104b1a260658c9f
Abhilash Bande <Abhilash.Bande@avid.com>
2026-05-07 17:03:36 +0530

    List : hetrogeneous list

--- scaler/13 List.py
+++ scaler/13 List.py
@@ -26,33 +26,41 @@
 
 
 ## List methods
-list = [100, 200, 100, 150, 250, 300, 100];
+# list = [100, 200, 100, 150, 250, 300, 100];
 
-print(list.count(100))
-print(list.index(250))  # if object is not in the list then it will throw an error
+# print(list.count(100))
+# print(list.index(250))  # if object is not in the list then it will throw an error
 
-print(list.pop())  # removes the last element
-print(list)
+# print(list.pop())  # removes the last element
+# print(list)
 
-list.remove(100) # removes the first occurrence of the element
-print(list)
+# list.remove(100) # removes the first occurrence of the element
+# print(list)
 
-# list.remove(20); # if the element is not in the list then it will throw an error
+# # list.remove(20); # if the element is not in the list then it will throw an error
 
-list.sort();
-print(list)
+# list.sort();
+# print(list)
 
-list.insert(2, 500) # inserts 500 at index 2
-print(list)
+# list.insert(2, 500) # inserts 500 at index 2
+# print(list)
 
-list.append(400) # adds 400 at the end of the list
-print(list)
+# list.append(400) # adds 400 at the end of the list
+# print(list)
 
-l1 = [111,222,333];
-list.append(l1) # adds the list l1 as a single element at the end of the list
-print(list)
+# l1 = [111,222,333];
+# list.append(l1) # adds the list l1 as a single element at the end of the list
+# print(list)
 
-l1.extend(list);
-print(l1)
+# l1.extend(list);
+# print(l1)
+
+
+# hetrogeneous list
+# list = [2, "Abhi", True, 3.14]
+
+# for i in list:
+#     print(type(i))
+
 
 
